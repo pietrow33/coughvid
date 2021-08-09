013a98b84644573146788fc13554370e3f76512a
Pietro Wong <pietrow.pw@gmail.com>
2021-08-09 15:19:11 -0300

    Streamlit configs

--- app.py
+++ app.py
@@ -10,21 +10,31 @@ from tensorflow.keras.backend import expand_dims
 import pickle
 from coughvid.DSP import classify_cough
 import os
+import subprocess
 
-
+#Recieve the loaded file from streamlit
 uploaded_file = st.file_uploader("Upload File")
 
-model = tf.keras.models.load_model('./notebooks/models_coughvid_model.h5')
+#Get file type, save and transform it to .wav if not already, then read audio
+file_type = str(uploaded_file)
+file_type = file_type[file_type.find("type")-7:file_type.find("type")-3]
+if file_type != ".wav":
+    with open(os.path.join("tempDir",f"test{file_type}"),"wb") as f: 
+        f.write(uploaded_file.getbuffer())
+    subprocess.call(["ffmpeg", "-i",f"./tempDir/test{file_type}", "./tempDir/test.wav"])
+    audio, rate = librosa.load("./tempDir/test.wav", sr=None)
+    os.remove(f"./tempDir/test{file_type}")
+    os.remove("./tempDir/test.wav")
+else:
+    audio, rate = librosa.load(uploaded_file, sr=None)
 
+#Load prediction model and cough detection model
+model = tf.keras.models.load_model('./notebooks/models_coughvid_model.h5')
 loaded_model = pickle.load(open(os.path.join('./models', 'cough_classifier'), 'rb'))
 loaded_scaler = pickle.load(open(os.path.join('./models','cough_classification_scaler'), 'rb'))
 
-audio, rate = librosa.load(uploaded_file)
-
 def cough_detect(audio, rate):
-    
     probability = classify_cough(audio, rate, loaded_model, loaded_scaler)
-    
     return probability
 
 def transform_audio(audio, rate):
@@ -35,38 +45,33 @@ def transform_audio(audio, rate):
     img = librosa.display.specshow(S_dB, sr=rate, fmax=8000, ax=ax)
     ax.axis("off")
 
-    plt.savefig('./front_end_pics/user8.png', bbox_inches='tight', pad_inches = 0)
-    
+    plt.savefig('./tempDir/user.png', bbox_inches='tight', pad_inches = 0)
     plt.close()
     
-    img2 = Image.open('./front_end_pics/user8.png')
+    img2 = Image.open('./tempDir/user.png')
     img2 = img2.resize((334, 217),Image.ANTIALIAS)
-    
-    img2.save('./front_end_pics/user9.png',optimize=True,quality=95)
-    
-    X = imageio.imread('./front_end_pics/user9.png')
-    
+    img2.save('./tempDir/user2.png',optimize=True,quality=95)
+    os.remove("./tempDir/user.png")    
+
+    X = imageio.imread('./tempDir/user2.png')
+    X = X / 255.
+    os.remove("./tempDir/user2.png")
+
     X = np.array(expand_dims(X, axis=0))
-    
     return X
 
+#Call cough detection function to get the probability of audio having cough
 probability = cough_detect(audio, rate)
 
+#Asks for new audio if cough detection level is below 60%, else predicts with the model to the given audio
 if probability <= 0.6:
-    st.write("Cough again bitch!")
-    st.write(probability)
+    st.write("Please record another audio. No cough detected.")
 else:
     X = transform_audio(audio, rate)
-
     y = model.predict(X)
-
     print(y)
 
     st.image(X)
-
     st.audio(uploaded_file)
-
-    st.write(y)
-
-    st.write(probability)
-    
+    st.write(f"The probability of having COVID through Audio analysis is: {(y[0][0]*100):.2f}%")
+    st.write(f"Classification: {np.round(y[0][0],0)}")
