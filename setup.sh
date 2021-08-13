d74723b18d681c21e9adb51bfa7bc87a331f8afd
Pietro Wong <pietrow.pw@gmail.com>
2021-08-13 18:39:52 -0300

    Last heroku try

--- setup.sh
+++ setup.sh
@@ -14,3 +14,15 @@ headless = true\n\
 enableCORS = false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+
+echo "[theme]
+primaryColor = ‘#84a3a7’
+backgroundColor = ‘#EFEDE8’
+secondaryBackgroundColor = ‘#fafafa’
+textColor= ‘#424242’
+font = ‘sans serif’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
